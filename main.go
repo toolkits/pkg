5f957c2924413de1531fa669efb4a88b5e03b5be
Ulric Qin <ulric.qin@gmail.com>
2018-12-06 14:50:31 +0800

    add net tools

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"pkg/list"
+	"pkg/slice"
 )
 
 func main() {
 	a := []int64{1, 3, 5, 56, 67, 7}
 	b := []int64{3, 5, 6, 7}
-	fmt.Println(list.Sub(a, b))
+	fmt.Println(slice.SubInt64(a, b))
 }

4fd4bcf755e4bcd3f583b09c2a47e9b23bd16f9d
710leo <710leo@gmail.com>
2019-03-14 18:09:36 +0800

    change sockets

--- nux/ss_s.go
+++ nux/ss_s.go
@@ -3,6 +3,8 @@ package nux
 import (
 	"bufio"
 	"bytes"
+	"io"
+	"io/ioutil"
 	"strconv"
 	"strings"
 
@@ -11,6 +13,34 @@ import (
 )
 
 func SocketStatSummary() (m map[string]uint64, err error) {
+	m = make(map[string]uint64)
+	bs, err := ioutil.ReadFile("/proc/net/sockstat")
+	if err != nil {
+		return
+	}
+	reader := bufio.NewReader(bytes.NewBuffer(bs))
+
+	for {
+		var lineBytes []byte
+		lineBytes, err = file.ReadLine(reader)
+		if err == io.EOF {
+			return
+		}
+		line := string(lineBytes)
+		s := strings.Split(line, " ")
+		if strings.HasPrefix(line, "sockets: used") {
+			m["sockets.used"], _ = strconv.ParseUint(s[2], 10, 64)
+		} else {
+			m["sockets.tcp.inuse"], _ = strconv.ParseUint(s[2], 10, 64)
+			m["sockets.tcp.timewait"], _ = strconv.ParseUint(s[6], 10, 64)
+			break
+		}
+	}
+
+	return
+}
+
+func ss() (m map[string]uint64, err error) {
 	m = make(map[string]uint64)
 	var bs []byte
 	bs, err = sys.CmdOutBytes("sh", "-c", "ss -s")

04741b043a8215e22aa057c89454b60e3314dfdb
Ulric Qin <ulric.qin@gmail.com>
2019-04-10 11:07:06 +0800

    add runner lib

---
+++ runner/runner.go
@@ -0,0 +1,32 @@
+package runner
+
+import (
+	"hash/crc32"
+	"log"
+	"math/rand"
+	"os"
+	"pkg/file"
+	"runtime"
+	"time"
+)
+
+var (
+	Hostname string
+	Cwd      string
+)
+
+func Init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	var err error
+	Hostname, err = os.Hostname()
+	if err != nil {
+		log.Fatalln("cannot get hostname")
+	}
+
+	Cwd = file.SelfDir()
+
+	rand.Seed(time.Now().UnixNano() + int64(os.Getpid()+os.Getppid()) + int64(crc32.ChecksumIEEE([]byte(Hostname))))
+}
